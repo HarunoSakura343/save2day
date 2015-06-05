27bc030e8b5ffcb354f57147628f42a5b1126b08
HarunoSakura343 <tim_pas@hotmail.com>
2015-06-05 13:36:26 +0200

    making the list view for selectable feeds

--- www/js/feed.js
+++ www/js/feed.js
@@ -1,46 +1,46 @@
 function Feed(name, url) {
-   var _db = window.localStorage;
-   var _tableName = 'feed';
+    var _db = window.localStorage;
+    var _tableName = 'feed';
 
-   this.name = name;
-   this.url = url;
+    this.name = name;
+    this.url = url;
 
-   this.save = function (feeds) {
-      _db.setItem(_tableName, JSON.stringify(feeds));
-   };
+    this.save = function (feeds) {
+        _db.setItem(_tableName, JSON.stringify(feeds));
+    };
 
-   this.load = function () {
-      return JSON.parse(_db.getItem(_tableName));
-   };
+    this.load = function () {
+        return JSON.parse(_db.getItem(_tableName));
+    };
 }
 
 Feed.prototype.add = function () {
-   var index = Feed.getIndex(this);
-   var feeds = Feed.getFeeds();
+    var index = Feed.getIndex(this);
+    var feeds = Feed.getFeeds();
 
-   if (index === false) {
-      feeds.push(this);
-   } else {
-      feeds[index] = this;
-   }
+    if (index === false) {
+        feeds.push(this);
+    } else {
+        feeds[index] = this;
+    }
 
-   this.save(feeds);
+    this.save(feeds);
 };
 
 Feed.prototype.delete = function () {
-   var index = Feed.getIndex(this);
-   var feeds = Feed.getFeeds();
+    var index = Feed.getIndex(this);
+    var feeds = Feed.getFeeds();
 
-   if (index !== false) {
-      feeds.splice(index, 1);
-      this.save(feeds);
-   }
+    if (index !== false) {
+        feeds.splice(index, 1);
+        this.save(feeds);
+    }
 
-   return feeds;
+    return feeds;
 };
 
- function timeSince(date) {
-     var datex = new Date(date);
+function timeSince(date) {
+    var datex = new Date(date);
     var seconds = Math.floor((new Date() - datex) / 1000);
 
     var interval = Math.floor(seconds / 31536000);
@@ -67,73 +67,95 @@ Feed.prototype.delete = function () {
     return Math.floor(seconds) + " seconde geleden";
 }
 Feed.prototype.compareTo = function (other) {
-   return Feed.compare(this, other);
+    return Feed.compare(this, other);
 };
 
 Feed.compare = function (feed, other) {
-   if (other == null) {
-      return 1;
-   }
-   if (feed == null) {
-      return -1;
-   }
-   var test = feed.name.localeCompare(other.name);
-   return (test === 0) ? feed.url.localeCompare(other.url) : test;
+    if (other == null) {
+        return 1;
+    }
+    if (feed == null) {
+        return -1;
+    }
+    var test = feed.name.localeCompare(other.name);
+    return (test === 0) ? feed.url.localeCompare(other.url) : test;
 };
 
 Feed.getFeeds = function () {
-   var feeds = new Feed().load();
-   return (feeds === null) ? [] : feeds;
+    var feeds = new Feed().load();
+    return (feeds === null) ? [] : feeds;
 };
 
 Feed.getFeed = function (feed) {
-   var index = Feed.getIndex(feed);
-   if (index === false) {
-      return null;
-   }
-   var feed = Feed.getFeeds()[index];
-   return new Feed(feed.name, feed.url);
+    var index = Feed.getIndex(feed);
+    if (index === false) {
+        return null;
+    }
+    var feed = Feed.getFeeds()[index];
+    return new Feed(feed.name, feed.url);
 };
 
 Feed.getIndex = function (feed) {
-   var feeds = Feed.getFeeds();
-   for (var i = 0; i < feeds.length; i++) {
-      if (feed.compareTo(feeds[i]) === 0) {
-         return i;
-      }
-   }
+    var feeds = Feed.getFeeds();
+    for (var i = 0; i < feeds.length; i++) {
+        if (feed.compareTo(feeds[i]) === 0) {
+            return i;
+        }
+    }
 
-   return false;
+    return false;
 };
 
 Feed.deleteFeeds = function () {
-   new Feed().save([]);
+    new Feed().save([]);
 };
 
 Feed.searchByName = function (name) {
-   var feeds = Feed.getFeeds();
-   for (var i = 0; i < feeds.length; i++) {
-      if (feeds[i].name === name) {
-         return new Feed(feeds[i].name, feeds[i].url);
-      }
-   }
+    var feeds = Feed.getFeeds();
+    for (var i = 0; i < feeds.length; i++) {
+        if (feeds[i].name === name) {
+            return new Feed(feeds[i].name, feeds[i].url);
+        }
+    }
 
-   return false;
+    return false;
 };
 
 Feed.searchByUrl = function (url) {
-   var feeds = Feed.getFeeds();
-   for (var i = 0; i < feeds.length; i++) {
-      if (feeds[i].url === url) {
-         return new Feed(feeds[i].name, feeds[i].url);
-      }
-   }
+    var feeds = Feed.getFeeds();
+    for (var i = 0; i < feeds.length; i++) {
+        if (feeds[i].url === url) {
+            return new Feed(feeds[i].name, feeds[i].url);
+        }
+    }
 
-   return false;
+    return false;
 };
 
 var onShake = function () {
-    navigator.notification.alert('thanks for shaking', function () {
+    navigator.notification.alert(checkConnection(), function () {
     }, 'Shake it');
 
 };
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+   return('Connection type: ' + states[networkState]);
+}
+function FeedToevoegen (Feednaam){
+    if(localstorage.getItem('feeds') === null){
+        localstorage.setItem('feeds', )
+        }
+    localStorage.setItem('feed', true);
+}
